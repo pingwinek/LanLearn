c6fc71bac5cd455432826e510f660fd8e79cd345
pingwinek <devmaterials@gmail.com>
2018-05-17 00:05:09 +0200

    Add LanLearn.Service project and TranslateService

---
+++ LanLearn/Dto/ITranslateService.cs
@@ -0,0 +1,12 @@
+﻿using Dto.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dto
+{
+    public interface ITranslateService
+    {
+        IEnumerable<EnglishDictionary> GetListWords(int account);
+    }
+}
---
+++ LanLearn/LanLearn.Service/TranslateService.cs
@@ -0,0 +1,16 @@
+﻿using Dto;
+using Dto.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LanLearn.Service
+{
+    public class TranslateService : ITranslateService
+    {
+        public IEnumerable<EnglishDictionary> GetListWords(int account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
